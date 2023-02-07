69d8879c8f5841633eff9958d702c1e3940d8551
Ahmet BICAKCI <119766247+ahmetbicakci@users.noreply.github.com>
2023-02-08 00:25:07 +0100

    branch v2

---
+++ src/B2.java
@@ -0,0 +1,27 @@
+public class B2 {
+    public static void main(String[] args) {
+
+
+        /* git ve gitHub icin 3 asama
+
+        1-projemiz/klsorimuz icin git'i tanistirma: git init
+
+        2-projemiz/klaosrumuz icin versiyon olusturma
+        git add .
+        git commit -m "Versiyon adi"
+
+        3- gitHub ile entegrasyon ve senkronozisyon
+            -ilk defa entegre etmek icin once gitHub'da yeni repo olusturup
+            oradaki son 3 adimi kopyalayip terminal yada CMD'ye yapistirip ENTER' a bas
+             -Ilk entegrasyondan sonra her degisiklik icin
+                - git add .
+                - git commit -m "versiyon adi"
+                - git push
+
+
+
+
+
+         */
+    }
+}
